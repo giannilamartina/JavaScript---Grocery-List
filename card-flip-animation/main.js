8d1683f534abf3a188d21d77b64bc0b7b05ad7a7
Gianni LaMartina <lamartinagianni@gmail.com>
2022-05-31 18:42:55 -0500

    customized card flips for prototype seo website

--- card-flip-animation/main.js
+++ card-flip-animation/main.js
@@ -1,4 +1,6 @@
-const card = document.querySelector(".card__inner");
-card.addEventListener("click", function () {
-  card.classList.toggle("is-flipped");
-});
+const card = document.querySelectorAll(".card__inner");
+
+function flipCard() {
+  this.classList.toggle("is-flipped");
+}
+card.forEach((card) => card.addEventListener("click", flipCard));
